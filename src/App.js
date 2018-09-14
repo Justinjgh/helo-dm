a3ee43897b5e3a957a9cdd1da1f82da0e5b8c4c9
Josiah Maina <josiah5115@gmail.com>
2018-09-14 10:26:00 -0600

    more changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Link } from 'react-router-dom';
+
 import './App.css';
+
+// import Auth from "./component/Auth/Auth";
+// import Post from "./component/Post/Post";
+// import Form from "./component/Form/Form";
+// import Dashboard from "./component/Dashboard/Dashboard";
+import Nav from "./component/Nav/Nav";
+import routes from "./routes";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+         {/* <nav className='nav'>
+          <Link to="/" className='links'>Auth</Link>
+          <Link to="/dashboard" className='links'>Dashboard</Link> 
+          <Link to="/post/:postid" className='links'>Post</Link> 
+          <Link to="/new" className='links'>Form</Link>
+         </nav> */}
+
+       <Nav />
+       { routes }
       </div>
     );
   }
-}
+};
 
 export default App;
+
+//  {/* {this.props.location.pathname === "/" ? null : <Nav />} */}
+//  {routes}
--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { HashRouter as Router, Route} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './ducks/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+   <Router>
+    <App />
+  </Router>
+  </Provider>
+,document.getElementById('root'));
